d1d92979d1d204576e04d8a2c838d1f1f77477e0
FedericoTentella <federicotentella@libero.it>
2016-11-16 20:27:45 +0100

    Update E3.cpp

--- E3.cpp
+++ E3.cpp
@@ -2,13 +2,14 @@
 #include <math.h>
 main()
 {
-	int Somma; //Somma dei Numeri//
+	int Somma; 
 	int I;
 	Somma=0;
 	I=1;
-	while(I<=100){
-	Somma=Somma+I;}
-	count<<"I=I+1"<<;
+	while(I<=100)
+	{
+	Somma=Somma+I;
+	I=I+1;
+	}
 	printf("\n Somma= %d",Somma);
 }
-
