765dc3f72537e0c7f301cca5b3f1bad7812105ad
FedericoTentella <federicotentella@libero.it>
2017-01-04 18:25:49 +0100

    Add files via upload

---
+++ E_1 Scambio di valori tra due variabili.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+	int A; // Primo valore
+	int B; // Secondo valore
+	int C; // Variabile di scambio
+		printf("\n Inserisci primo valore\n");
+		scanf("%d",& A);
+		printf("\n Inserisci secondo valore\n");
+		scanf("%d",& B);
+		C=B;
+		B=A;
+		A=C;
+		printf("\n I due valori sono: %d   %d ", A,B);
+		
+	}
+
+	
+
